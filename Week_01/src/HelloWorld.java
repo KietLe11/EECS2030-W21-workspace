5be90bd553ae873a4868df28899c7838969ccb78
KietLe11 <77179362+KietLe11@users.noreply.github.com>
2021-09-09 23:07:40 -0400

    Update HelloWorld.java
    nothing much

--- Week_01/src/HelloWorld.java
+++ Week_01/src/HelloWorld.java
@@ -2,9 +2,12 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Hello Bwear");
 		System.out.println("Hello Bwear Pawww");
+		
+		
+	
 	}
 
 }
